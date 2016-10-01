0b9322936c88680a3375ccab9fbee2b0b52fa14f
Hadrien Gardeur <hadrien.gardeur@feedbooks.com>
2016-10-01 15:29:24 +0200

    Added a Service Worker to the Moby Dick example

---
+++ examples/MobyDick/sw.js
@@ -0,0 +1,39 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open('pub-static-v1').then(c => c.addAll([
+      './',
+      'index.html',
+      'html/title-page.html',
+      'html/copyright.html',
+      'html/introduction.html',
+      'html/epigraph.html',
+      'html/c001.html',
+      'html/c002.html',
+      'html/c003.html',
+      'html/c004.html',
+      'html/c005.html',
+      'html/c006.html',
+      'css/mobydick.css',
+      'fonts/STIXGeneral.otf',
+      'fonts/STIXGeneralBol.otf',
+      'fonts/STIXGeneralBolIta.otf',
+      'fonts/STIXGeneralItalic.otf'
+    ]))
+  );
+});
+
+self.addEventListener('activate', event => {
+  clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  const sameOrigin = url.origin === location.origin;
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
---
+++ examples/MobyDick/webpub.js
@@ -0,0 +1,11 @@
+(function() {
+	if ('serviceWorker' in navigator) {
+  	navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    	// Registration was successful
+  	  console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+	  }).catch(function(err) {
+    	// registration failed :(
+    	console.log('ServiceWorker registration failed: ', err);
+  	});
+	}
+});
