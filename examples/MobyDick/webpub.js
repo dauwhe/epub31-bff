32252518fc3fa38933d6e727a305dde66e6977d8
Hadrien Gardeur <hadrien.gardeur@feedbooks.com>
2016-10-01 18:03:41 +0200

    Renamed the Service Worker JS

--- examples/MobyDick/webpub.js
+++ examples/MobyDick/webpub.js
@@ -1,15 +1,12 @@
 (function() {
-      var installingMsg = document.querySelector('.installing-msg');
-      if (!navigator.serviceWorker) {
-        installingMsg.textContent = "Service worker not supported";
-        return;
-      }
+  var installingMsg = document.querySelector('.installing-msg');
+  if (!navigator.serviceWorker) {
+    installingMsg.textContent = "Service worker not supported";
+    return;
+  }
       
-      // I'm not a fan of depending on SW for page render
-      // but it I don't think there's another way around
-      // it here.
-      navigator.serviceWorker.register('sw.js');
-      navigator.serviceWorker.ready.then(function() {
-        console.log('ready');
-      });
-    }());
+  navigator.serviceWorker.register('serviceworker.js');
+  navigator.serviceWorker.ready.then(function() {
+    console.log('ready');
+  });
+}());
