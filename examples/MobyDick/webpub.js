c54d6c8aadd088483e20878a5caea5585f4991b8
Hadrien Gardeur <hadrien.gardeur@feedbooks.com>
2016-10-01 15:35:01 +0200

    Changed path to SW

--- examples/MobyDick/webpub.js
+++ examples/MobyDick/webpub.js
@@ -1,8 +1,8 @@
 (function() {
 	if ('serviceWorker' in navigator) {
-  	navigator.serviceWorker.register('/sw.js').then(function(registration) {
+  	navigator.serviceWorker.register('sw.js').then(function(registration) {
     	// Registration was successful
-  	  console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  	  console.log('ServiceWorker registration successful with scope: ', registration.scope);
 	  }).catch(function(err) {
     	// registration failed :(
     	console.log('ServiceWorker registration failed: ', err);
