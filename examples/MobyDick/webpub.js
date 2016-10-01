304e315890e79660396818d37ea8e12be68c7796
Hadrien Gardeur <hadrien.gardeur@feedbooks.com>
2016-10-01 15:44:23 +0200

    Now that the SW work, revert to external file

--- examples/MobyDick/webpub.js
+++ examples/MobyDick/webpub.js
@@ -1,11 +1,16 @@
 (function() {
-	if ('serviceWorker' in navigator) {
-  	navigator.serviceWorker.register('sw.js').then(function(registration) {
-    	// Registration was successful
-  	  console.log('ServiceWorker registration successful with scope: ', registration.scope);
-	  }).catch(function(err) {
-    	// registration failed :(
-    	console.log('ServiceWorker registration failed: ', err);
-  	});
-	}
-});
+      var installingMsg = document.querySelector('.installing-msg');
+      if (!navigator.serviceWorker) {
+        installingMsg.textContent = "Service worker not supported";
+        return;
+      }
+      
+      // I'm not a fan of depending on SW for page render
+      // but it I don't think there's another way around
+      // it here.
+      navigator.serviceWorker.register('sw.js');
+      navigator.serviceWorker.ready.then(function() {
+        console.log('ready');
+        location.reload();
+      });
+    }());
