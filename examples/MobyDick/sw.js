f398e9166e31220c1cb4e08e797a6038b291eb23
Hadrien Gardeur <hadrien.gardeur@feedbooks.com>
2016-10-01 15:47:55 +0200

    Updated cache key for SW

--- examples/MobyDick/sw.js
+++ examples/MobyDick/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open('pub-static-v1').then(c => c.addAll([
+    caches.open('pub-static-v2').then(c => c.addAll([
       './',
       'index.html',
       'html/title-page.html',
